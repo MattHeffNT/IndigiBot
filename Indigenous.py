f15729bffe66353d225ea91c147485adc31ba981
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-07-21 02:03:58 +0930

    Update Indigenous.py

--- Indigenous.py
+++ Indigenous.py
@@ -18,7 +18,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 
-#################### static twitter search  way ######################
+#################### static twitter search  ######################
 
 def store_last_id(tweet_id):
     """ Store a tweet id in a file """
@@ -30,7 +30,6 @@ def get_last_id():
     """ Read the last retweeted id from a file """
     with open("lastid", "r") as fp:
         return fp.read()
-
 
 if __name__ == '__main__':
 
@@ -47,10 +46,7 @@ if __name__ == '__main__':
         
         str1= "indigenous"
         str2= "https://twitter.com/i/web/status/"
-        str3= ". Indigenous"
-
         lastid= get_last_id()
-
 
         #if the tweet object has the retweeted_status attribute and isn't a reply to someone (can change this second condition later if we like) then store username in 
         # variable "user" and the dictionary that's in a list that's in a tuple inside the "url" variable.
@@ -68,17 +64,10 @@ if __name__ == '__main__':
 
                         if link not in lastid and str2 in link:
                 
-
                 #tweet the person with our message then store the tweet id
                 # to make sure we aren't duplicating tweets.
 
                                 api.update_status ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X "+ link)
                                 store_last_id(link)              
 
-
-    ## Things left to do:
-
-    ## finishing migration to PythonAnywhere and set scheduler
-
-            
-
+        
