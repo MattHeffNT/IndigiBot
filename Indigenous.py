69d0987382d19e7af6b31a16548bac6fdf39e53f
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-07-22 23:41:19 +0930

    updated IndigiBot
    - added a dictionary of terms to check to make sure I'm not @ing people who are using indigenous correctly
    - I think India just launched a space craft "Chandrayaan"..... so there have been a bunch of tweets about the "indigenous" technology. I've just added a filter to exclude these tweets.
    - IndigiBot now @s both Retweeted and non-Retweeted statuses
    - added push notification so that I'm notified when Indigibot has been run.

--- Indigenous.py
+++ Indigenous.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3.64
 import tweepy
 from tweepy import OAuthHandler
-
+from notify_run import Notify
 
 CONSUMER_KEY = 'x'
 CONSUMER_SECRET = 'x'
 ACCESS_TOKEN = 'x'
 ACCESS_TOKEN_SECRET = 'x'
 
-
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-
 # Create API object
 api = tweepy.API(auth)
+notify = Notify()
 
-
-#################### static twitter search  ######################
+#################### static twitter search  way ######################
 
 def store_last_id(tweet_id):
     """ Store a tweet id in a file """
     with open("lastid","a") as fp:
-        fp.write(str(tweet_id)+'\n')
+        fp.write(str(tweet_id)+""+'\n')
 
 
 def get_last_id():
     """ Read the last retweeted id from a file """
     with open("lastid", "r") as fp:
         return fp.read()
+
 
 if __name__ == '__main__':
 
@@ -39,39 +38,86 @@ if __name__ == '__main__':
         print("No retweet yet")
         last_id = None
 
-    for tweet in tweepy.Cursor(api.search, q="indigenous", since_id=last_id).items(150):
+    for tweet in tweepy.Cursor(api.search, q="indigenous", since_id=last_id).items(400):
 
-         #str1 variable stores indigenous as a string variable so we can use it in twitter searches, 
-         # can add complexity here later (i.e. lowercase aboriginal, add dictionaries of terms to iterate through searches  etc)
-         # will also consider adding further context analysis and or machine learning capabilities (dictionary values of sentences or
-         # adjective and noun pairs we want to exclude. Str2 is variable to check if links we will extract later are correct. 
+         #str1 variable is literally just looking for lowercase indigenous in twitter searches, can add complexity here (i.e. lowercase aboriginal etc)
+         # will also consider adding further context analysis and or machine learning capabilities   
         
         str1= "indigenous"
-        str2= "https://twitter.com/user/status/"
 
-        #if the tweet object has the retweeted_status attribute and isn't a reply to someone (can change this second condition later if we like) then store username in 
-        #variable "user" and the dictionary that's in a list that's in a tuple inside the "url" variable.
-        #and last condition checks if the word indigenous lowercase (str1) is in the twitter post
+        lastid= get_last_id()
 
-        if hasattr(tweet,'retweeted_status') and tweet.in_reply_to_screen_name==None and str1 in (f'{tweet.text}'):
-                user=tweet.retweeted_status.author.screen_name
-                url=tweet.retweeted_status.entities['urls']
+        # Some context to try and minimize false positives.
 
-                # loop through url dictionary until we get the expanded url value then store this in the link variable. Then replace
-                # the text with generic twitter url so that it appears as media in the bot's feed. (tweepy doesn't have quote tweet
-                # functionality just yet
-                
-                for urls in url:
-                        link=urls['expanded_url']
-                        link=link.replace("https://twitter.com/i/web/status/","https://twitter.com/user/status/")
+  
 
-                # double check bot hasn't already tweeted the current post in memory also double check that "link" is correct url type
+        context_dictionary={" plants"," animals"," species"," herbs"," fruits"," birds"," mammals"," fish"," to"," space technology"," space programme"}
+
+        for i in context_dictionary:
+
+                acceptable= str1+i
+        
+                #if the tweet object has the retweeted_status attribute and isn't a reply to someone (can change this second condition later if we like) then store username in 
+                # variable "user" and the dictionary that's in a list that's in a tuple inside the "url" variable.
+                if acceptable and "Chandrayaan" in (f'{tweet.text}') :
+                        break
+
+
+                elif hasattr(tweet,'retweeted_status') and tweet.in_reply_to_screen_name==None and str1 in (f'{tweet.text}'):
+                        user=tweet.retweeted_status.author.screen_name
+                        url=tweet.retweeted_status.entities['urls']
+
+                        # loop through url dictionary until we get the expanded url value then store this in the link variable.
+
+                        for urls in url:
+                                global link
+                                link = urls['expanded_url']
+                                link=link.replace("https://twitter.com/i/web/status/","https://twitter.com/user/status/")
+
+                        str2= "https://twitter.com/user/status/"
+
+
                         if link not in lastid and str2 in link:
-                
+                                
                 #tweet the person with our message then store the tweet id
                 # to make sure we aren't duplicating tweets.
 
-                                api.update_status ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X "+ link)
-                                store_last_id(link)              
+                                api.update_status ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
+                                # print ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
+                                # print ("with RT status")
+                                store_last_id(link)
+                                break
+                                
+                        else:
+
+                                continue
+
+                #new parameter to capture more tweets..same as above except for not RT tweets
+
+                elif hasattr(tweet,'retweeted_status')==False and tweet.in_reply_to_screen_name==None and str1 in (f'{tweet.text}'):
+                        link= "https://twitter.com/user/status/"+tweet.id_str
+                        user= tweet.user.screen_name
+
+                       
+        #if statement "if the word indigenous in the twitter post and bot hasn't already tweeted then execute nested if statement
+
+                        str2= "https://twitter.com/user/status/"                
+
+                        if link not in lastid and str2 in link:
+                        
+
+                #tweet the person with our message then store the tweet id
+                # to make sure we aren't duplicating tweets.
+
+                                api.update_status ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
+                                # print ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
+                                # print ("without RT status")
+                                store_last_id(link)
+                                break
+                                
+
+                        else:
+                                continue
+
 print ("done")
-        
+notify.send('IndigiBot has finished running')
