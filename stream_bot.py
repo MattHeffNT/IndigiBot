0729fa4464399b68593f83365721ecd86ff71c5c
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-08-01 22:54:39 +0930

    fixes to acceptable strings
    Also some better error handling.

--- stream_bot.py
+++ stream_bot.py
@@ -1,7 +1,7 @@
 import tweepy
 from tweepy import OAuthHandler,Stream
-from notify_run import Notify
 from tweepy.streaming import StreamListener
+from time import sleep
 
 CONSUMER_KEY = 'x'
 CONSUMER_SECRET = 'x'
@@ -30,14 +30,16 @@ def store_last_id(tweet_id):
         fp.write(str(tweet_id)+""+'\n')
 
 str1="indigenous"
+
 lastid= get_last_id()
 
 class MyStreamListener(tweepy.StreamListener):
 
-    
+
     def on_status(self, tweet):
 
-        context_dictionary={" plants"," animals"," species"," herbs"," fruits"," birds"," mammals"," fish"," to"," space technology"," space programme"}
+        context_dictionary={" plants"," animals"," reptiles" " species"," herbs"," fruits"," birds"," mammals"," fish"," to", " to "," variety"," flora"," fauna"," British"," Brits"}
+
 
         for i in context_dictionary:
 
@@ -53,44 +55,33 @@ class MyStreamListener(tweepy.StreamListener):
 
                 #we don't want to capture retweeted tweets for live streamed.
 
-                elif hasattr(tweet,'retweeted_status')==False and tweet.in_reply_to_screen_name==None and str1 in (f'{tweet.text}'):
-                        link= "https://twitter.com/user/status/"+tweet.id_str
-                        user= tweet.user.screen_name
+        if hasattr(tweet,'retweeted_status')==False and tweet.in_reply_to_screen_name==None and str1 in tweet.text:
+                link= "https://twitter.com/user/status/"+tweet.id_str
+                tweetID= tweet.id_str
+                user= tweet.user.screen_name
 
 
         #if statement "if the word indigenous in the twitter post and bot hasn't already tweeted then execute nested if statement
 
-                        str2= "https://twitter.com/user/status/"
+                str2= "https://twitter.com/user/status/"
 
-                        if link not in lastid and str2 in link:
+                if link not in lastid and str2 in link:
 
                 #tweet the person with our message then store the tweet id
                 # to make sure we aren't duplicating tweets.
 
-                                #api.update_status ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
-
-                                print ("@"+user+" should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
-                                # print ("without RT status")
+                                sleep(60)
+                                # api.update_status ("@"+user+" Should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
+                                # print ("@"+user+" Should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
+                                api.retweet(tweetID)
                                 store_last_id(link)
-                                break
-
-
-                        else:
-                                continue
-
 
     def on_error(self, status_code):
-        if status_code == 420:
+        if status_code == 420 or status_code==185:
             #returning False in on_error disconnects the stream
             return False
         # returning non-False reconnects the stream, with backoff.
-    
 
 myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-
-myStream.filter(track=[str1],is_async=True)
-
-
-
-
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener,wait_on_rate_limit=True)
+myStream.filter(track=[str1],async=True)
