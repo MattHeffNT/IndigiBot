6e4aea0b798e43714d0452ca971d34100bb2d507
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-08-07 14:20:03 +0930

    added Torres Straight filter and error fixes
    - The bot now picks up when people incorrectly spell the The Torres Strait Islands
    - Also fixed name error

--- stream_bot.py
+++ stream_bot.py
@@ -29,9 +29,12 @@ def store_last_id(tweet_id):
     with open("lastid","a") as fp:
         fp.write(str(tweet_id)+""+'\n')
 
+
 str1="indigenous"
-bbc="Brazil's indigenous people: Miners kill one in invasion of protected reserve"
-guardian="Brazil's indigenous people, miners"
+str3="torres straight"
+notweet= ""
+acceptable= ""
+
 
 
 lastid= get_last_id()
@@ -39,31 +42,35 @@ lastid= get_last_id()
 class MyStreamListener(tweepy.StreamListener):
 
 
-
     def on_status(self, tweet):
+
+        str1="indigenous"
+        str4="torres straight"
+        notweet= ""
+        acceptable= ""
 
         context_dictionary={" plants"," animals"," reptiles" " species"," herbs"," fruits"," birds"," mammals"," fish"," to", " to "," variety"," flora"," fauna"," British"," Brits","brit","Brit"}
 
         collocations= {"chandrayaan","Chandrayaan","ISRO","miners kill", "Brazil murder", "indigenously", "indigenous to","indigenous red grape","indigenous grape","Amazon gold miners","indigenous/native plants","indigenous cows","Brazil miners"}
 
-        #loops to check both collocations and just general strings
 
         for i in context_dictionary:
-
                 acceptable= str1+i
 
-
-        for i in collocations:
-
-            if i in tweet.text:
-
-                    noTweet= i
-
-
                 #if the tweet object has the retweeted_status attribute and isn't a reply to someone (can change this second condition later if we like) then store username in
-                # variable "user". Then check if dictionaries above are in the tweet.
-         
+                # variable "user" and the dictionary that's in a list that's in a tuple inside the "url" variable.
+
+        for word in collocations:
+            if word in tweet.text:
+                    noTweet= word
+        else:
+                noTweet=""
+
+
+                #we don't want to capture retweeted tweets for live streamed, this condition also checks for strings in dictionaries above.
+
         if hasattr(tweet,'retweeted_status')==False and tweet.in_reply_to_screen_name==None and str1 in tweet.text and acceptable not in tweet.text and noTweet not in tweet.text:
+
                 link= "https://twitter.com/user/status/"+tweet.id_str
                 tweetID= tweet.id_str
                 user= tweet.user.screen_name
@@ -79,10 +86,34 @@ class MyStreamListener(tweepy.StreamListener):
                 # to make sure we aren't duplicating tweets.
 
                                 sleep(60)
-                                # api.update_status ("@"+user+" Should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
-                                # print ("@"+user+" Should you be using a capital I for Indigenous? #uppercaseBlacks https://t.co/3G7C7l7y4X  "+ link)
                                 api.retweet(tweetID)
                                 store_last_id(link)
+
+
+        # if the tweet has both torres straight and indeigenous then retweet them
+
+        elif hasattr(tweet,'retweeted_status')==False and tweet.in_reply_to_screen_name==None and str1 in tweet.text and noTweet not in tweet.text and acceptable not in tweet.text and str3.title() in tweet.text or str3 in tweet.text or str3.upper() in tweet.text or str3.capitalize() in tweet.text:
+                link= "https://twitter.com/user/status/"+tweet.id_str
+                tweetID= tweet.id_str
+                str2= "https://twitter.com/user/status/"
+
+                if link not in lastid and str2 in link:
+                                sleep(60)
+                                api.retweet(tweetID)
+                                store_last_id(link)
+
+        # if the tweet has torres straight then retweet them
+
+        elif hasattr(tweet,'retweeted_status')==False and tweet.in_reply_to_screen_name==None and str3.title() in tweet.text or str3 in tweet.text or str3.upper() in tweet.text or str3.capitalize() in tweet.text:
+                link= "https://twitter.com/user/status/"+tweet.id_str
+                tweetID= tweet.id_str
+                str2= "https://twitter.com/user/status/"
+
+                if link not in lastid and str2 in link:
+                                sleep(60)
+                                api.retweet(tweetID)
+                                store_last_id(link)
+
 
     def on_error(self, status_code):
         if status_code == 420 or status_code==185:
@@ -92,4 +123,4 @@ class MyStreamListener(tweepy.StreamListener):
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener,wait_on_rate_limit=True)
-myStream.filter(track=[str1],async=True)
+myStream.filter(track=['indigenous,torres straight'],async=True)
