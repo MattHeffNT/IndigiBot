b3f2ae6d8dc92b7049d091efe277a743192d5a1c
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-11-14 04:52:00 +0930

    fixed wrong terms for variables

--- stream_bot.py
+++ stream_bot.py
@@ -43,10 +43,10 @@ class MyStreamListener(tweepy.StreamListener):
         notweet= ""
         acceptable= ""
 
-        context_dictionary={(" plants"," animals"," reptiles" " species"," herbs"," fruits"," birds"," mammals"," fish",
+        collocations={(" plants"," animals"," reptiles" " species"," herbs"," fruits"," birds"," mammals"," fish",
                              " to", " to "," variety"," flora"," fauna"," British"," Brits","brit","Brit")}
 
-        collocations= {("chandrayaan","Chandrayaan","ISRO","miners kill", "Brazil murder", "indigenously", 
+        context_dictionary= {("chandrayaan","Chandrayaan","ISRO","miners kill", "Brazil murder", "indigenously", 
                         "indigenous to","indigenous red grape","indigenous grape","Amazon gold miners","indigenous/native plants",
                         "indigenous cows","Brazil miners")}
 
